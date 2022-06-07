3e0e51ae7ea9b5171b79cc0ca23c5a50722dfc7a
sevgiaydn <sevgiaydin1999@hotmail.com>
2022-06-07 23:31:40 +0300

    Initial commit

---
+++ src/main/java/com/designpatterns/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.designpatterns.demo;
+
+import com.designpatterns.demo.singleton.SingletonExample;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		SingletonExample singletonObj1 = SingletonExample.getInstance();
+		SingletonExample singletonObj2 = SingletonExample.getInstance();
+
+		System.out.println("singletonObj1 ==> " + singletonObj1);
+		System.out.println("singletonObj2 ==> " + singletonObj2);
+
+		System.out.println(singletonObj1 == singletonObj2);
+	}
+
+}
---
+++ src/main/java/com/designpatterns/demo/singleton/SingletonExample.java
@@ -0,0 +1,14 @@
+package com.designpatterns.demo.singleton;
+
+public class SingletonExample {
+    private static SingletonExample instance;
+
+    private SingletonExample(){}
+
+    public static SingletonExample getInstance(){
+        if (instance == null)
+            instance = new SingletonExample();
+
+        return instance;
+    }
+}
