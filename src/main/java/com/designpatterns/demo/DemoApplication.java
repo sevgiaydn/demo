9775b62b6d67478f71f1dde0af3396326f8f7620
sevgiaydn <sevgiaydin1999@hotmail.com>
2022-06-07 23:42:17 +0300

    test

--- src/main/java/com/designpatterns/demo/DemoApplication.java
+++ src/main/java/com/designpatterns/demo/DemoApplication.java
@@ -10,13 +10,13 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		SingletonExample singletonObj1 = SingletonExample.getInstance();
+		/*SingletonExample singletonObj1 = SingletonExample.getInstance();
 		SingletonExample singletonObj2 = SingletonExample.getInstance();
 
 		System.out.println("singletonObj1 ==> " + singletonObj1);
 		System.out.println("singletonObj2 ==> " + singletonObj2);
 
-		System.out.println(singletonObj1 == singletonObj2);
+		System.out.println(singletonObj1 == singletonObj2);*/
 	}
 
 }
